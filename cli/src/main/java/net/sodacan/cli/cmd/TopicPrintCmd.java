5f52c50a334f8b1ab0a89d0ce0ac0f061dfb6fcc
John Churin <john.churin@gmail.com>
2023-02-09 15:10:17 -0800

    New repository

---
+++ cli/src/main/java/net/sodacan/cli/cmd/TopicPrintCmd.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 John M Churin
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sodacan.cli.cmd;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Map;
+
+import org.apache.commons.cli.CommandLine;
+
+import net.sodacan.cli.Action;
+import net.sodacan.cli.CmdBase;
+import net.sodacan.cli.CommandContext;
+import net.sodacan.config.Config;
+import net.sodacan.messagebus.MB;
+import net.sodacan.messagebus.MBRecord;
+import net.sodacan.messagebus.MBTopic;
+import net.sodacan.mode.Mode;
+
+public class TopicPrintCmd extends CmdBase implements Action, PropertyChangeListener {
+
+	public TopicPrintCmd( CommandContext cc) {
+		super( cc );
+	}
+	
+	@Override
+	public void execute(CommandLine commandLine, int index) {
+		init( commandLine, index);
+		String topicName = needArg(0, "topic name");
+		Mode mode = needMode();
+		MB mb = mode.getMessageBusService().getMB(Config.getInstance());
+		MBTopic mbt = mb.openTopic(topicName, 0);
+		Map<String, MBRecord> map = mbt.snapshot();
+		map.forEach((k,v) -> System.out.println(v));
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		System.out.println(event.getPropertyName() + " = " + event.getNewValue());
+	}
+
+}
---
+++ cli/src/main/java/net/sodacan/cli/cmd/TopicStatusCmd.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 John M Churin
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sodacan.cli.cmd;
+
+import org.apache.commons.cli.CommandLine;
+
+import net.sodacan.SodacanException;
+import net.sodacan.cli.Action;
+import net.sodacan.cli.CmdBase;
+import net.sodacan.cli.CommandContext;
+import net.sodacan.config.Config;
+import net.sodacan.messagebus.MB;
+import net.sodacan.mode.Mode;
+
+public class TopicStatusCmd extends CmdBase implements Action {
+
+	public TopicStatusCmd( CommandContext cc) {
+		super( cc );
+	}
+
+	@Override
+	public void execute(CommandLine commandLine, int index) {
+		init(commandLine, index);
+		Mode mode = needMode();
+		MB mb = mode.getMessageBusService().getMB(Config.getInstance());
+		String topicName = null;
+		try {
+			topicName = this.needArg(0, "topic name");
+			System.out.println(mb.describeTopic(topicName));
+		} catch (Exception e) {
+			throw new SodacanException("Problem getting topic description: " + topicName, e);
+		}
+	}
+}
